02b871e7c6a47cae20825dea5aa9e0050bb18988
brooks <myt2000@126.com>
2022-09-23 14:29:35 +0800

    fragment的动态切换

--- app/src/main/java/com/brooks/fragmenttest/MainActivity.kt
+++ app/src/main/java/com/brooks/fragmenttest/MainActivity.kt
@@ -2,10 +2,24 @@ package com.brooks.fragmenttest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.left_fragment.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        button.setOnClickListener {
+            replaceFragment(AnotherRightFragment())
+        }
+        replaceFragment(RightFragment())
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+        // 不是很理解supportFragmentManager是如何被引用到的
+        val fragmentManager = supportFragmentManager
+        val transaction = fragmentManager.beginTransaction()
+        transaction.replace(R.id.rightLayout, fragment)
+        transaction.commit()
     }
 }
